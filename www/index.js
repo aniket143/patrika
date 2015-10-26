55cc5389cebf6f6b7fac7d3f33eea7a13782b593
aniket143 <aniket143@gmail.com>
2015-10-26 17:58:13 +0530

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,5 +1,6 @@
 var channel = '';
 var t = document.getElementById('temperature');
+	
 
 var pubnub = PUBNUB.init({
         subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
@@ -10,13 +11,15 @@ function initialize() {
     bindEvents();
 }
 function bindEvents() {
+
     document.addEventListener('deviceready', main, false);
 }
 
 function main() {
+
     var pushNotification = window.plugins.pushNotification;
+   pushNotification.register(successHandler, errorHandler, {'senderID':'837099162939','ecb':'onNotificationGCM'});
    // pushNotification.register(successHandler, errorHandler, {'senderID':'1010018101342','ecb':'onNotificationGCM'});
-    pushNotification.register(successHandler, errorHandler, {'senderID':'837099162939','ecb':'onNotificationGCM'});
 }
 
 function successHandler(result) {
@@ -28,6 +31,7 @@ function errorHandler(error) {
 }
 
 function onNotificationGCM(e) {
+			alert('hello');
     switch( e.event ){
         case 'registered':
             if ( e.regid.length > 0 ){
@@ -55,6 +59,7 @@ function onNotificationGCM(e) {
 
 // Publish the channel name and regid to PubNub
 function registerDevice(regid) {
+				alert('hellooooo');
     channel = regid.substr(regid.length - 8).toLowerCase();
 
     var c = document.querySelector('.channel');
